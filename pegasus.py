b2dc58eca4e591a0dda6d89cce0dd479f56101a6
azycn <50297583+azycn@users.noreply.github.com>
2023-03-01 18:50:19 -0800

    pegasus billsum on CA test (split)

--- pegasus.py
+++ pegasus.py
@@ -5,14 +5,16 @@ from datasets import load_dataset
 from tqdm import tqdm
 
 def evaluate():
-    dataset = load_dataset('billsum', split="test")
+    # dataset = load_dataset('billsum', split="test")
+    dataset = load_dataset('billsum', split="ca_test")
+
     '''txt_path = "/kaggle/input/feedback-prize-2021/train/0000D23A521A.txt"
 
     with open(txt_path, "rt") as f:
         txt = f.read()'''
-    tests = dataset["text"]
-    #tests = tests[0:5]
-    #print(tests[0])
+    
+    tests = dataset["text"][865:1051]
+
     model_name = "google/pegasus-xsum"
     device = "cuda" if torch.cuda.is_available() else "cpu"
     tokenizer = PegasusTokenizer.from_pretrained(model_name)
@@ -21,7 +23,7 @@ def evaluate():
     batch = tokenizer(tests, truncation=True, padding="longest", return_tensors="pt").to(device)
     translated = model.generate(**batch)
     tgt_text = tokenizer.batch_decode(translated, skip_special_tokens=True)
-    text_file = open("summaries1.txt", "w")
+    text_file = open("summaries_catest.txt", "w")
     text_file.write("==================================".join(tgt_text))
 
     text_file.close()
