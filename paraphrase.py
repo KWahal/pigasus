9a483eb42df4498d9e210b2768984a16739bedf6
KWahal <karsen.wahal@gmail>
2023-03-17 23:26:03 -0700

    paraphrased

--- paraphrase.py
+++ paraphrase.py
@@ -6,7 +6,7 @@ torch_device = 'cuda' if torch.cuda.is_available() else 'cpu'
 tokenizer = PegasusTokenizer.from_pretrained(model_name)
 model = PegasusForConditionalGeneration.from_pretrained(model_name).to(torch_device)
 
-SEPARATOR = "=================================="
+SEPARATOR = "=============================="
 num_beams = 1
 num_return_sequences = 1
 #input_text = "This bill authorizes the Contra Costa Transportation Authority to conduct a pilot project for the testing of autonomous vehicles that do not have a driver seated in the driver seat and are not equipped with a steering wheel, a brake pedal, or an accelerator. The testing shall be conducted only at a privately owned business park designated by the authority, inclusive of public roads within the designated business park, and at GoMentum Station located within the boundaries of the former Concord Naval Weapons Station. The autonomous vehicle shall operate at speeds of less than 35 miles per hour. Before the testing of an autonomous vehicle that does not have a driver seated in the driver seat, the operator, and a combination of the two, shall: obtain an instrument of insurance, surety bond, or proof of self-insurance in an amount of five million dollars $5 million; and provide evidence of such insurance to the California Department of Motor Vehicles."
@@ -27,24 +27,34 @@ def get_response(input_text,num_return_sequences,num_beams):
   batch = tokenizer([input_text],truncation=True,padding='longest',max_length=60, return_tensors="pt").to(torch_device)
   translated = model.generate(**batch,max_length=60,num_beams=num_beams, num_return_sequences=num_return_sequences, temperature=1.5)
   tgt_text = tokenizer.batch_decode(translated, skip_special_tokens=True)
+  tgt_text = ''.join(tgt_text)
   return tgt_text
 
-original_texts = "".join(open('summaries/val_ca_sum_10_100_REAL2.txt', 'r').readlines())
-original_texts = original_texts.split(SEPARATOR)[:-1]
+original_texts = "".join(open('gform_for_paraphrase.txt', 'r').readlines())
+original_texts = original_texts.split(SEPARATOR)[1:]
 
-original_texts = original_texts[:100]
+# original_texts_new = []
+# for text in original_texts:
+#   text = text.split(SEPARATOR2)[1:]
+#   print(text)
+#   for section in text:
+#     original_texts_new.append(section)
 
 all_paraphrased = []
 for text in tqdm(original_texts):
+  
   sentences = text.split(".")
-  sentences = sentences[:len(sentences)-1]
+  print(text[-1])
+  if text[-1] == ".":
+    sentences = sentences[:len(sentences)-1]
   paraphrased = []
   for sentence in sentences:
     paraphrased.append(get_response(sentence, num_return_sequences, num_beams))
-  all_paraphrased.append(paraphrased)
+  paraphrases = ' '.join(paraphrased)
+  all_paraphrased.append(paraphrases)
 
 for paraphrase in all_paraphrased:
   print(SEPARATOR)
-  for sentence in paraphrase:
-    print(sentence)
+  print(paraphrase)
+
 
