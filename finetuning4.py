36dbe7a42746ab012e0cb613b87d5e8770a334a0
ehejin <109569513+ehejin@users.noreply.github.com>
2023-03-19 01:42:04 -0700

    Update finetuning4.py

--- finetuning4.py
+++ finetuning4.py
@@ -1,20 +1,3 @@
-"""Script for fine-tuning Pegasus
-Example usage:
-  # use XSum dataset as example, with first 1000 docs as training data
-  from datasets import load_dataset
-  dataset = load_dataset("xsum")
-  train_texts, train_labels = dataset['train']['document'][:1000], dataset['train']['summary'][:1000]
-
-  # use Pegasus Large model as base for fine-tuning
-  model_name = 'google/pegasus-large'
-  train_dataset, _, _, tokenizer = prepare_data(model_name, train_texts, train_labels)
-  trainer = prepare_fine_tuning(model_name, tokenizer, train_dataset)
-  trainer.train()
-
-Reference:
-  https://huggingface.co/transformers/master/custom_datasets.html
-"""
-
 from transformers import PegasusForConditionalGeneration, PegasusTokenizer, Trainer, TrainingArguments
 import torch
 from tqdm import tqdm
